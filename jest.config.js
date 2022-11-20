27add3318d40612f5b2d9f1928b2ce25ec1e7ae2
gobackk1 <k.kwbt4@icloud.com>
2022-11-20 23:19:02 +0900

    テストとSFCを１つにまとめる設定

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: "@vue/cli-plugin-unit-jest",
+  moduleFileExtensions: ["js", "json", "vue"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["/tests/e2e"]
 }
