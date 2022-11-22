0b37cdffec1e16fb3a38f7bd18e598e665d29c4d
gobackk1 <k.kwbt4@icloud.com>
2022-11-22 16:03:23 +0900

    テストカバレッジを表示する

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   moduleFileExtensions: ["js", "json", "vue"],
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-  testPathIgnorePatterns: ["/tests/e2e"]
+  testPathIgnorePatterns: ["/tests/e2e"],
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**/*.{js,vue}", "!**/node_modules/**", "!./src/**/*.(stories|test|spec).js"]
 }
