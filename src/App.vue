6c1ac5548a6ea3485eb0660efdb277c87a02a5a8
gobackk1 <k.kwbt4@icloud.com>
2022-11-26 17:17:59 +0900

    App更新 - commons.scssとicon.scssをリンク - router-link削除

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-@import '@/styles/reset.css';
+@import "@/styles/reset.css";
+@import "@/styles/common.scss";
+@import "@/styles/icons.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
