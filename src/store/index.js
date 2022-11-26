48d8209048132e3bebf2b61a7417b0a5a56b9b9e
gobackk1 <k.kwbt4@icloud.com>
2022-11-26 16:23:30 +0900

    リスト表示切り替えに関わるstore実装

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentListView: "column"
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    toggleListView(state) {
+      state.currentListView = state.currentListView === "grid" ? "column" : "grid"
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
