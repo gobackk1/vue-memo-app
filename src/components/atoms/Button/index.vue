b8fde6fe389e08d7d69d9dc3c0d1a4bb36926641
gobackk1 <k.kwbt4@icloud.com>
2022-11-26 16:19:35 +0900

    Button実装

---
+++ src/components/atoms/Button/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <button :class="buttonClasses" @click="onClickButton"><slot /></button>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    onClickButton: Function,
+    color: {
+      type: String,
+      default: "primary"
+    }
+  },
+  computed: {
+    buttonClasses() {
+      return `root root-${this.color}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/property";
+
+.root {
+  border-radius: $radius;
+  font-size: $font-size-m;
+  color: $color-text;
+  cursor: pointer;
+  padding: $space;
+  &-primary {
+    background-color: $color-primary;
+  }
+  &-warning {
+    background-color: $color-warning;
+  }
+  &-danger {
+    background-color: $color-danger;
+  }
+}
+</style>
