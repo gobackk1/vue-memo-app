957f452516689e73898329c2d19907904e420d89
gobackk1 <k.kwbt4@icloud.com>
2022-11-28 16:21:04 +0900

    Txtコンポーネント実装

---
+++ src/components/atoms/Txt/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="tag" :class="classes">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: "AppTxt",
+  props: {
+    role: String,
+    size: {
+      type: String,
+      default: "m"
+    },
+    tag: {
+      type: String,
+      default: "p"
+    }
+  },
+  computed: {
+    classes() {
+      let classes = []
+      if (this.role) classes.push(`color-${this.role}`)
+      if (this.size) classes.push(`size-${this.size}`)
+      return classes.join(" ")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
