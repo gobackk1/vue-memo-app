03980267f2cde1efae08453b12dce514b06c6944
gobackk1 <k.kwbt4@icloud.com>
2022-11-29 14:11:37 +0900

    メモ作成インプット実装

---
+++ src/components/molecules/CreateMemoInput/index.spec.js
@@ -0,0 +1,74 @@
+import CreateMemoInput from "./index"
+import { mount } from "@vue/test-utils"
+
+describe("CreateMemoInput", () => {
+  describe("props", () => {
+    it("createMemoの引数のチェック", async () => {
+      const createMemo = jest.fn()
+      const memoData = {
+        memoTitle: "title",
+        memoBody: "body"
+      }
+      const wrapper = mount(CreateMemoInput, {
+        propsData: {
+          createMemo
+        }
+      })
+      await wrapper.setData({ isEditing: true, ...memoData })
+      await wrapper.findComponent({ ref: "create-button" }).trigger("click")
+      expect(createMemo).toBeCalledWith(memoData)
+    })
+  })
+  describe("ユーザーアクション", () => {
+    const createMemo = jest.fn()
+    const memoData = {
+      memoTitle: "title",
+      memoBody: "body"
+    }
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = mount(CreateMemoInput, {
+        propsData: {
+          createMemo
+        },
+        attachTo: document.body
+      })
+    })
+
+    afterEach(() => {
+      wrapper.destroy()
+    })
+
+    it("インプットにフォーカスすると、表示が追加されること", async () => {
+      await wrapper.setData({ isEditing: false, ...memoData })
+      await wrapper.findComponent({ ref: "title" }).trigger("focus")
+      const buttonWrapper = wrapper.findComponent({ ref: "create-button" })
+      expect(wrapper.findComponent({ ref: "body" }).isVisible()).toBe(true)
+      expect(buttonWrapper.isVisible()).toBe(true)
+    })
+    it("インプットにフォーカスすると、テキストエリアにフォーカスが移ること", async () => {
+      await wrapper.findComponent({ ref: "title" }).trigger("focus")
+      expect(document.activeElement.className).toContain("input-body")
+    })
+    it("コンポーネントの外をクリックすると、インプット以外が非表示になること", async () => {
+      await wrapper.setData({ isEditing: true, ...memoData })
+      const div = document.createElement("div")
+      div.className = "mock-element"
+      document.body.appendChild(div)
+      await document.querySelector(".mock-element").click()
+      expect(wrapper.findComponent({ ref: "body" }).isVisible()).toBe(false)
+      expect(wrapper.findComponent({ ref: "create-button" }).isVisible()).toBe(false)
+    })
+    it("タイトルと本文が未入力の時は、ボタンを無効にすること", async () => {
+      await wrapper.setData({ isEditing: true, memoTitle: "", memoBody: "" })
+      expect(wrapper.findComponent({ ref: "create-button" }).attributes().disabled).not.toBe(undefined)
+    })
+    it("タイトルか本文かのどちらかが入力してある時、ボタンを有効にすること", async () => {
+      await wrapper.setData({ isEditing: true, memoTitle: "title", memoBody: "" })
+      expect(wrapper.findComponent({ ref: "create-button" }).attributes().disabled).toBe(undefined)
+      await wrapper.setData({ isEditing: true, memoTitle: "", memoBody: "body" })
+      expect(wrapper.findComponent({ ref: "create-button" }).attributes().disabled).toBe(undefined)
+    })
+  })
+})
---
+++ src/components/molecules/CreateMemoInput/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div :class="classes">
+      <input
+        type="text"
+        v-model="memoTitle"
+        placeholder="メモのタイトルを入力..."
+        @focus="onFocusInput"
+        class="input-title"
+        ref="title"
+      />
+      <div v-show="isEditing" class="body">
+        <textarea v-model="memoBody" rows="3" ref="body" placeholder="メモ本文を入力..." class="input-body"></textarea>
+        <Button :onClickButton="onClickCreate" :disabled="disabled" ref="create-button">作成</Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "@/components/atoms/Button"
+export default {
+  name: "CreateMemoInput",
+  components: { Button },
+  props: {
+    createMemo: Function
+  },
+  data() {
+    return {
+      isEditing: false,
+      memoTitle: "",
+      memoBody: ""
+    }
+  },
+  methods: {
+    onFocusInput() {
+      if (!this.isEditing) {
+        this.isEditing = true
+        this.$nextTick(() => {
+          this.$refs.body.focus()
+        })
+      }
+    },
+    onClickCreate() {
+      this.createMemo({
+        memoTitle: this.memoTitle,
+        memoBody: this.memoBody
+      })
+      this.memoTitle = ""
+      this.memoBody = ""
+      this.isEditing = false
+    },
+    closeTextarea(ev) {
+      if (!this.$el.contains(ev.target)) {
+        this.isEditing = false
+      }
+    }
+  },
+  computed: {
+    disabled() {
+      return this.memoTitle === "" && this.memoBody === ""
+    },
+    classes() {
+      return this.isEditing ? ["root", "is-editing"] : ["root"]
+    }
+  },
+  mounted() {
+    window.addEventListener("click", this.closeTextarea)
+  },
+  beforeDestroy() {
+    window.removeEventListener("click", this.closeTextarea)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/property";
+
+.root {
+  border: $border;
+  padding: $space;
+  border-radius: $radius;
+}
+
+.input-title {
+  width: 100%;
+  .is-editing & {
+    margin-bottom: $space;
+  }
+}
+
+.input-body {
+  width: 100%;
+  margin-bottom: $space;
+}
+
+.body {
+  text-align: right;
+}
+</style>
