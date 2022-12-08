3d5273d746b54a93eed71d990820c513ffb34972
gobackk1 <k.kwbt4@icloud.com>
2022-12-08 17:37:01 +0900

    定数追加

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,12 @@
 export const APP_PREFIX = "App--"
+export const FIRESTORE_API_ORIGIN = process.env.VUE_APP_FIRESTORE_API_ORIGIN
+export const PROJECT_ID = process.env.VUE_APP_PROJECT_ID
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCUZVux1YiBBsej6muTDrrQFclXeSjNKqE",
+  authDomain: "vue-memo-app-bd2b4.firebaseapp.com",
+  projectId: "vue-memo-app-bd2b4",
+  storageBucket: "vue-memo-app-bd2b4.appspot.com",
+  messagingSenderId: "692445313126",
+  appId: "1:692445313126:web:6ce12cd2ab2f7cd4ac15a7",
+  measurementId: "G-KGRB0MTZ7T"
+}
